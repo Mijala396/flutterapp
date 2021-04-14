701db6dfd8514415fe335b76fcc42bbb1a5ad53a
Mijala Manandhar <mijala900@gmail.com>
2021-04-14 22:29:55 +0545

    changes

--- lib/Tutor/Profile/tutor_profile.dart
+++ lib/Tutor/Profile/tutor_profile.dart
@@ -5,18 +5,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class TutorProfile extends StatefulWidget {
-
   @override
   _TutorProfileState createState() => _TutorProfileState();
 }
 
 class _TutorProfileState extends State<TutorProfile> {
   String firstName = '';
-  String lastName= '';
+  String lastName = '';
   String email = '';
   String number = '';
   String address = '';
-  String educationalInstitute='';
+  String educationalInstitute = '';
   String academicleveltoteach = '';
   int chargePerHour;
   List subjects;
@@ -26,12 +25,11 @@ class _TutorProfileState extends State<TutorProfile> {
     final pref = await SharedPreferences.getInstance();
     final token = pref.getString('token');
 
-
     final Response response = await get(
       'http://10.0.2.2:8000/auth/getUsers/',
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization':"Bearer $token"
+        'Authorization': "Bearer $token"
       },
     );
 
@@ -45,18 +43,17 @@ class _TutorProfileState extends State<TutorProfile> {
       academicleveltoteach = data[0]['academicleveltoteach'];
       address = data[0]['address'];
       chargePerHour = data[0]['chargePerHour'];
-      subjects =data[0]['subjects'];
+      subjects = data[0]['subjects'];
     });
     print(data);
-
   }
-
 
   @override
   void initState() {
     getUserInfo();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,161 +65,168 @@ class _TutorProfileState extends State<TutorProfile> {
           elevation: 0,
         ),
         floatingActionButton: SpeedDial(
-              overlayOpacity: 0,
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.blueAccent,
-              closeManually: true,
-              elevation: 8,
-              children: [
-                SpeedDialChild(
-                  child: Icon(Icons.edit),
-                  label: "Edit Profile",
-                  onTap: (){
-                    Navigator.pushNamed(context, '/tutorEdit');
-                  }
-                ),
-                SpeedDialChild(
-                    child: Icon(Icons.book),
-                    label: "Add Subject",
-                    onTap: (){
-                      Navigator.pushNamed(context, '/addSubject');
-                    }
-                ),
-                SpeedDialChild(
-                    child: Icon(Icons.menu),
-                    label: "Manage Subjects",
-                    onTap: (){
-                      Navigator.pushNamed(context, '/manageSubjects',
-                      arguments: {
-                        'subjects':subjects
-                      }
-                      );
-                    }
-                ),
-              ],
+          overlayOpacity: 0,
+          backgroundColor: Colors.pink,
+          foregroundColor: Colors.pinkAccent,
+          closeManually: true,
+          elevation: 8,
+          children: [
+            SpeedDialChild(
+                child: Icon(Icons.edit),
+                label: "Edit Profile",
+                onTap: () {
+                  Navigator.pushNamed(context, '/tutorEdit');
+                }),
+            SpeedDialChild(
+                child: Icon(Icons.book),
+                label: "Add Subject",
+                onTap: () {
+                  Navigator.pushNamed(context, '/addSubject');
+                }),
+            SpeedDialChild(
+                child: Icon(Icons.menu),
+                label: "Manage Subjects",
+                onTap: () {
+                  Navigator.pushNamed(context, '/manageSubjects',
+                      arguments: {'subjects': subjects});
+                }),
+          ],
         ),
-        body:Padding(
+        body: Padding(
           padding: EdgeInsets.all(30.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Center(
                 child: CircleAvatar(
-                  backgroundImage: NetworkImage('https://i1.wp.com/recommendmeanime.com/wp-content/uploads/2017/01/best-anime-teachers.jpg?fit=1360%2C768&ssl=1'),
+                  backgroundImage: NetworkImage(
+                      'https://i1.wp.com/recommendmeanime.com/wp-content/uploads/2017/01/best-anime-teachers.jpg?fit=1360%2C768&ssl=1'),
                   radius: 40.0,
-
                 ),
               ),
               Divider(
                 height: 90,
                 color: Colors.red[900],
               ),
-
               Text(
                 'Name',
                 style: TextStyle(
-                  color:Colors.grey,
+                  color: Colors.white,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(height: 10,),
-
+              SizedBox(
+                height: 10,
+              ),
               Text(
                 '$firstName $lastName',
                 style: TextStyle(
-                  color:Colors.amber[200],
+                  color: Colors.white,
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Text(
                 'Teacher for',
                 style: TextStyle(
-                  color:Colors.grey,
+                  color: Colors.white,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(height: 10,),
-
-
-
+              SizedBox(
+                height: 10,
+              ),
               Text(
                 '$academicleveltoteach',
                 style: TextStyle(
-                  color:Colors.amber[200],
+                  color: Colors.white,
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Text(
                 'Graduated From',
                 style: TextStyle(
-                  color:Colors.grey,
+                  color: Colors.white,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(height: 10,),
-
-
-
+              SizedBox(
+                height: 10,
+              ),
               Text(
                 '$educationalInstitute',
                 style: TextStyle(
-                  color:Colors.amber[200],
+                  color: Colors.white,
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Row(
                 children: <Widget>[
                   Icon(
                     Icons.email,
-                    color: Colors.grey[400],
+                    color: Colors.white,
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Text(
                     '$email',
                     style: TextStyle(
-                      color: Colors.grey[400],
+                      color: Colors.white,
                       fontSize: 18.0,
                       letterSpacing: 2,
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Row(
                 children: <Widget>[
                   Icon(
                     Icons.phone,
-                    color: Colors.grey[400],
+                    color: Colors.white,
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Text(
                     '$number',
                     style: TextStyle(
-                      color: Colors.grey[400],
+                      color: Colors.white,
                       fontSize: 18.0,
                       letterSpacing: 2,
                     ),
                   ),
                 ],
               ),
-
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Row(
                 children: <Widget>[
                   Icon(
                     Icons.location_city,
-                    color: Colors.grey[400],
+                    color: Colors.white,
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Text(
                     '$address',
                     style: TextStyle(
@@ -233,37 +237,44 @@ class _TutorProfileState extends State<TutorProfile> {
                   ),
                 ],
               ),
-
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Row(
                 children: <Widget>[
                   Icon(
                     Icons.monetization_on,
                     color: Colors.green[400],
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Text(
                     '$chargePerHour per hour',
                     style: TextStyle(
-                      color: Colors.grey[400],
+                      color: Colors.white,
                       fontSize: 18.0,
                       letterSpacing: 2,
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Row(
                 children: <Widget>[
                   Icon(
                     Icons.account_circle,
-                    color: Colors.green[400],
+                    color: Colors.green,
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Text(
                     'Currently Active',
                     style: TextStyle(
-                      color: Colors.grey[400],
+                      color: Colors.white,
                       fontSize: 18.0,
                       letterSpacing: 2,
                     ),
@@ -272,8 +283,6 @@ class _TutorProfileState extends State<TutorProfile> {
               )
             ],
           ),
-
-        )
-    );
+        ));
   }
 }
--- lib/Tutor/TutorClass/tutor_class.dart
+++ lib/Tutor/TutorClass/tutor_class.dart
@@ -10,31 +10,126 @@ class TutorClass extends StatefulWidget {
 
 class _TutorClassState extends State<TutorClass> {
   List sessions;
-  Future<void> getTutorSessions()async{
+  Future<void> getTutorSessions() async {
     final pref = await SharedPreferences.getInstance();
     final token = pref.getString('token');
 
     final Response response = await get(
-        'http://10.0.2.2:8000/auth/session-approve/',
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-          'Authorization':"Bearer $token"
-        },
+      'http://10.0.2.2:8000/auth/session-approve/',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': "Bearer $token"
+      },
     );
 
     sessions = jsonDecode(response.body);
 
     print(sessions);
-
   }
 
   @override
-  void initState()  {
+  void initState() {
     getTutorSessions();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('session requests'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: sessions
+                .map<Widget>((item) => Column(
+                      children: <Widget>[
+                        Card(
+                          margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: <Widget>[
+                                Text(
+                                  'Name : ${item['student_name']} ',
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.grey[800]),
+                                ),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  'Subject : ${item['subject']}',
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.grey[800]),
+                                ),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  'email : ${item['student_email']} ',
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.grey[800]),
+                                ),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  'session_date : ${item['session_date']}',
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.grey[800]),
+                                ),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  'session_time : ${item['session_time']}',
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.grey[800]),
+                                ),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  'message : ${item['message']}',
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.grey[800]),
+                                ),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  'duration : ${item['session_duration']} ',
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.grey[800]),
+                                ),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    FlatButton.icon(
+                                        onPressed: () {
+                                          //getAcceptrequest(item['data']);
+                                        },
+                                        label: Text('Accept request'),
+                                        icon: Icon(Icons.info)),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ))
+                .toList(),
+          ),
+        ));
   }
 }
